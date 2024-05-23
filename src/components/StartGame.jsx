2d3320c579a2924664c919c79b47820ce82d9c63
Sapna190 <sapnasharma8849711@gmail.com>
2024-05-23 23:17:17 +0530

    Update .gitignore and StartGame component

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -9,6 +9,7 @@ const StartGame = ({toggle}) => {
            <div><img src="/images/dices.png" /></div>
            <div className="content">
             <h1>Dice Game</h1>
+            <h4>~By Sapna Sharma</h4>
             <Button onClick={toggle}>
                 Play Now</Button>
            </div>
@@ -24,11 +25,22 @@ const Container = styled.div`
     display: flex;
     margin: 0 auto;
     align-items: center;
+    
 
     .content {
         h1{
             font-size: 96px;
             white-space: nowrap;
+            margin-bottom: 0 ;
+        }
+
+        h4{
+            display: flex;
+            margin-left: 350px;
+            margin-bottom: 50px;
+            margin-top: 0;
+            white-space: nowrap;
+            align-self: flex-end;
         }
 
     }
